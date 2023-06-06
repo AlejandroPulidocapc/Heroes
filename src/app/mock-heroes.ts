4fede8cd33f14c3bf4a160118d2b987b7c7793bc
AlejandroPulidocapc <pulidocerinza@gmail.com>
2023-06-05 20:58:53 -0500

    Finalización de proyecto

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr. Nice', poder:'fuerza'},
+  { id: 13, name: 'Bombasto', poder:'fuerza' },
+  { id: 14, name: 'Celeritas' , poder:'fuerza'},
+  { id: 15, name: 'Magneta' , poder:'fuerza'},
+  { id: 16, name: 'RubberMan', poder:'fuerza' },
+  { id: 17, name: 'Dynama' , poder:'fuerza'},
+  { id: 18, name: 'Dr. IQ' , poder:'fuerza'},
+  { id: 19, name: 'Magma' , poder:'fuerza'},
+  { id: 20, name: 'Tornado', poder:'fuerza' }
+];
+
